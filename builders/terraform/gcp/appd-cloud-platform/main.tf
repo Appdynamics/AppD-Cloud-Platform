ae1cda086aef950994698537be17f747c6e9e172
Ed Barberis <ed.barberis@gmail.com>
2020-11-02 13:26:47 -0500

    Upgraded Terraform GCP provider to 3.45.

--- builders/terraform/gcp/appd-cloud-platform/main.tf
+++ builders/terraform/gcp/appd-cloud-platform/main.tf
@@ -5,7 +5,7 @@ terraform {
 
 # Providers ----------------------------------------------------------------------------------------
 provider "google" {
-  version = ">= 3.44"
+  version = ">= 3.45"
 
   project = var.gcp_project_id
   region  = var.gcp_region
