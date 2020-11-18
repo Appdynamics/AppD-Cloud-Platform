9141b1baa6f5f18d88520bbd59b018812e4dd9e4
Ed Barberis <ed.barberis@gmail.com>
2020-11-18 10:44:58 -0500

    Upgraded Terraform AWS provider to 3.15.

--- builders/terraform/aws/appd-cloud-platform/main.tf
+++ builders/terraform/aws/appd-cloud-platform/main.tf
@@ -5,7 +5,7 @@ terraform {
 
 # Providers ----------------------------------------------------------------------------------------
 provider "aws" {
-  version = ">= 3.13"
+  version = ">= 3.15"
   region  = var.aws_region
 }
 
