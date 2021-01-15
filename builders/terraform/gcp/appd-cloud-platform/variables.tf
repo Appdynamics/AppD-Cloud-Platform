c0747c8fb7debd5fa241c9b9d9abcce413fcea07
Ed Barberis <ed.barberis@gmail.com>
2021-01-15 18:32:07 -0500

    Upgraded Terraform GCP provider to 3.52.

--- builders/terraform/gcp/appd-cloud-platform/variables.tf
+++ builders/terraform/gcp/appd-cloud-platform/variables.tf
@@ -98,7 +98,7 @@ variable "gcp_source_image_family" {
 variable "gcp_source_image" {
   description = "The source disk image."
   type        = string
-  default     = "appd-cloud-platform-20115-ha-centos79-2021-01-04"
+  default     = "appd-cloud-platform-20115-ha-centos79-2021-01-15"
 # default     = "centos-7-v20200910"
 }
 
--- builders/terraform/gcp/appd-cloud-platform/versions.tf
+++ builders/terraform/gcp/appd-cloud-platform/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = ">= 3.51"
+      version = ">= 3.52"
     }
 
     null = {
