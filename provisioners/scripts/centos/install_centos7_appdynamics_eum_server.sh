f1934374aaf30e89b81307b739e2d3258267d9d6
Ed Barberis <ed.barberis@gmail.com>
2021-04-19 15:22:12 -0400

    Upgraded AppDynamics EUM Server to 21.4.0.32860.

--- provisioners/scripts/centos/install_centos7_appdynamics_eum_server.sh
+++ provisioners/scripts/centos/install_centos7_appdynamics_eum_server.sh
@@ -34,8 +34,8 @@ appd_eum_private_host="${appd_eum_private_host:-}"
 # appd platform install parameters.
 appd_home="${appd_home:-/opt/appdynamics}"
 appd_eum_server_home="${appd_eum_server_home:-eum}"
-appd_eum_server_release="${appd_eum_server_release:-21.2.0.32701}"
-appd_eum_server_sha256="${appd_eum_server_sha256:-c6d0e43c7501f4d89886e890dd874e6a5535931652636ef99d1cf6afd3f4029f}"
+appd_eum_server_release="${appd_eum_server_release:-21.4.0.32860}"
+appd_eum_server_sha256="${appd_eum_server_sha256:-a2b42b669747d9c94fd9b59df5f9802a3d8ac3b16904b97ef7bd7e80ab74dc2e}"
 appd_platform_user_name="${appd_platform_user_name:-centos}"
 appd_platform_user_group="${appd_platform_user_group:-centos}"
 appd_eum_server_jvm_xms="${appd_eum_server_jvm_xms:-1024}"
@@ -74,9 +74,9 @@ Usage:
   [OPTIONAL] appdynamics platform install parameters [w/ defaults].
     [root]# export appd_home="/opt/appdynamics"                         # [optional] appd home (defaults to '/opt/appdynamics').
     [root]# export appd_eum_server_home="eum"                           # [optional] platform home folder (defaults to 'eum').
-    [root]# export appd_eum_server_release="21.2.0.32701"               # [optional] platform release (defaults to '21.2.0.32701').
+    [root]# export appd_eum_server_release="21.4.0.32860"               # [optional] platform release (defaults to '21.4.0.32860').
                                                                         # [optional] platform sha-256 checksum (defaults to published value).
-    [root]# export appd_eum_server_sha256="c6d0e43c7501f4d89886e890dd874e6a5535931652636ef99d1cf6afd3f4029f"
+    [root]# export appd_eum_server_sha256="a2b42b669747d9c94fd9b59df5f9802a3d8ac3b16904b97ef7bd7e80ab74dc2e"
     [root]# export appd_platform_user_name="appd"                       # [optional] platform user name (defaults to 'appd').
     [root]# export appd_platform_user_group="appd"                      # [optional] platform group (defaults to 'appd').
     [root]# export appd_eum_server_jvm_xms=1024"                        # [optional] platform JVM XMS (defaults to '1024').
