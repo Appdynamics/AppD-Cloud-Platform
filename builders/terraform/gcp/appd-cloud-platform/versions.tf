84b698bce4ab4f54403541fbd6407fe9effeafba
Ed Barberis <ed.barberis@gmail.com>
2022-10-19 16:40:23 -0400

    Upgraded Terraform to 1.3.3; and providers to AWS 4.35 and GCP 4.41.

--- builders/terraform/gcp/appd-cloud-platform/versions.tf
+++ builders/terraform/gcp/appd-cloud-platform/versions.tf
@@ -1,11 +1,11 @@
 # Terraform ----------------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.3.1"
+  required_version = ">= 1.3.3"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.39, < 5.0"
+      version = ">= 4.41, < 5.0"
     }
 
     null = {
--- provisioners/scripts/common/install_hashicorp_terraform.sh
+++ provisioners/scripts/common/install_hashicorp_terraform.sh
@@ -28,9 +28,9 @@
 #---------------------------------------------------------------------------------------------------
 
 # install hashicorp terraform. ---------------------------------------------------------------------
-terraform_release="1.3.1"
+terraform_release="1.3.3"
 terraform_binary="terraform_${terraform_release}_linux_amd64.zip"
-terraform_sha256="0847b14917536600ba743a759401c45196bf89937b51dd863152137f32791899"
+terraform_sha256="fa5cbf4274c67f2937cabf1a6544529d35d0b8b729ce814b40d0611fd26193c1"
 
 # create local bin directory (if needed).
 mkdir -p /usr/local/bin
