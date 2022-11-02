c5b55ee595c7930e51bfb1d94c989f0576b9f76c
Ed Barberis <ed.barberis@gmail.com>
2022-11-02 12:33:21 -0400

    Upgraded Terraform to 1.3.3; and providers to AWS 4.37 and GCP 4.42.

--- builders/terraform/aws/appd-cloud-platform/main.tf
+++ builders/terraform/aws/appd-cloud-platform/main.tf
@@ -36,7 +36,7 @@ data "aws_ami" "appd_cloud_platform_ha_centos79" {
 # Modules ------------------------------------------------------------------------------------------
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = ">= 3.16"
+  version = ">= 3.18"
 
   name = "VPC-${var.resource_name_prefix}-${local.current_date}"
   cidr = var.aws_vpc_cidr_block
@@ -55,7 +55,7 @@ module "vpc" {
 
 module "security_group" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = ">= 4.13"
+  version = ">= 4.16"
 
   name        = "SG-${var.resource_name_prefix}-${local.current_date}"
   description = "Security group for example usage with EC2 instance"
