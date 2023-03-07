26a2e97bf35920b86c58344e6d90f2c4a76593a9
Ed Barberis <ed.barberis@gmail.com>
2023-03-07 15:41:31 -0500

    Upgraded AppD Events Service to 23.2.0.131.

--- provisioners/scripts/centos/install_centos7_appdynamics_events_service.sh
+++ provisioners/scripts/centos/install_centos7_appdynamics_events_service.sh
@@ -119,15 +119,25 @@ ${appd_platform_folder}/platform-admin/bin/platform-admin.sh \
 ${appd_platform_folder}/platform-admin/bin/platform-admin.sh list-hosts
 
 # install appdynamics events service. --------------------------------------------------------------
-set +x  # temporarily turn command display OFF.
+#####set +x  # temporarily turn command display OFF.
+#####${appd_platform_folder}/platform-admin/bin/platform-admin.sh \
+#####  install-events-service \
+#####    --platform-name "${appd_platform_name}" \
+#####    --profile "${appd_events_service_profile}" \
+#####    --hosts "${appd_events_service_host1}" \
+#####            "${appd_events_service_host2}" \
+#####            "${appd_events_service_host3}"
 ${appd_platform_folder}/platform-admin/bin/platform-admin.sh \
-  install-events-service \
+  submit-job \
     --platform-name "${appd_platform_name}" \
-    --profile "${appd_events_service_profile}" \
-    --hosts "${appd_events_service_host1}" \
-            "${appd_events_service_host2}" \
-            "${appd_events_service_host3}"
-set -x  # turn command display back ON.
+    --service events-service \
+    --job install \
+    --target-version "latest" \
+    --args profile="${appd_events_service_profile}" \
+           serviceActionHost="${appd_events_service_host1}" \
+           serviceActionHost="${appd_events_service_host2}" \
+           serviceActionHost="${appd_events_service_host3}"
+#####set -x  # turn command display back ON.
 
 # verify installation.
 ${appd_platform_folder}/platform-admin/bin/platform-admin.sh show-events-service-health
