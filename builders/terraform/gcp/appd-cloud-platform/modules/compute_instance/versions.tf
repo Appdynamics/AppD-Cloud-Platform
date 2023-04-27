886dcecb3438b6f30439277803bf0afe7c36e944
Ed Barberis <ed.barberis@gmail.com>
2023-04-27 12:01:39 -0400

    Upgraded Terraform to 1.4.6; and providers to AWS 4.64 and GCP 4.63.

--- builders/terraform/gcp/appd-cloud-platform/modules/compute_instance/versions.tf
+++ builders/terraform/gcp/appd-cloud-platform/modules/compute_instance/versions.tf
@@ -15,11 +15,11 @@
  */
 
 terraform {
-  required_version = ">= 1.4.5"
+  required_version = ">= 1.4.6"
   required_providers {
-    google = ">= 4.62, < 5.0"
+    google = ">= 4.63, < 5.0"
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-vm:compute_instance/v8.0.0"
+    module_name = "blueprints/terraform/terraform-google-vm:compute_instance/v8.0.1"
   }
 }
