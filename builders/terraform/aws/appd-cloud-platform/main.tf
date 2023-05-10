bbc0e047260196f18abfd7370872314a4a1764ec
Ed Barberis <ed.barberis@gmail.com>
2023-05-10 09:56:04 -0400

    Fixed VPC module issue by setting 'manage_default_network_acl' flag to 'false'.

--- builders/terraform/aws/appd-cloud-platform/main.tf
+++ builders/terraform/aws/appd-cloud-platform/main.tf
@@ -80,10 +80,11 @@ module "vpc" {
   public_subnets  = var.aws_vpc_public_subnets
   private_subnets = var.aws_vpc_private_subnets
 
-  enable_nat_gateway   = true
-  enable_vpn_gateway   = false
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_nat_gateway         = true
+  single_nat_gateway         = true
+  enable_dns_hostnames       = true
+  manage_default_network_acl = false
+  map_public_ip_on_launch    = true
 
   tags = local.resource_tags
 }
