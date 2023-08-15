18f7117a64e2f6c7fa5fea9d9edd11102a6bf26b
Ed Barberis <ed.barberis@gmail.com>
2023-08-15 15:12:32 -0400

    Upgraded AppDynamics Enterprise Console to 23.7.0.10073.

--- builders/terraform/aws/appd-cloud-platform/variables.tf
+++ builders/terraform/aws/appd-cloud-platform/variables.tf
@@ -80,7 +80,7 @@ variable "aws_ec2_ssh_pub_key_name" {
 variable "aws_ec2_source_ami_filter" {
   description = "AWS EC2 source AMI disk image filter."
   type        = string
-  default     = "AppD-Cloud-Platform-2340-HA-CentOS79-AMI-*"
+  default     = "AppD-Cloud-Platform-2370-HA-CentOS79-AMI-*"
 }
 
 variable "aws_ec2_instance_type" {
