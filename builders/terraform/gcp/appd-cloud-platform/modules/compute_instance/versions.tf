afc5f28fc30e1eb7993d283dc4bad71e48cb6b63
Ed Barberis <ed.barberis@gmail.com>
2023-09-12 15:36:28 -0400

    Upgraded Terraform to 1.5.7; and providers to AWS 5.16 and GCP 4.82.

--- builders/terraform/gcp/appd-cloud-platform/modules/compute_instance/versions.tf
+++ builders/terraform/gcp/appd-cloud-platform/modules/compute_instance/versions.tf
@@ -17,7 +17,7 @@
 terraform {
   required_version = ">= 1.5.7"
   required_providers {
-    google = ">= 4.81, < 5.0"
+    google = ">= 4.82, < 5.0"
   }
   provider_meta "google" {
     module_name = "blueprints/terraform/terraform-google-vm:compute_instance/v9.0.0"
--- builders/terraform/gcp/appd-cloud-platform/versions.tf
+++ builders/terraform/gcp/appd-cloud-platform/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.81, < 5.0"
+      version = ">= 4.82, < 5.0"
     }
 
     null = {
