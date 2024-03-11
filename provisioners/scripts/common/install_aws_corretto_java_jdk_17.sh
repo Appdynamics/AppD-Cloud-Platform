6d171be2e7d658296787254e0ae511fb102fdb50
Ed Barberis <ed.barberis@gmail.com>
2024-03-11 15:33:45 -0400

    Upgraded support for Amazon Corretto JDK 8u402, 11.0.22, 17.0.10, and 21.0.2.

--- provisioners/scripts/common/install_aws_corretto_java_jdk_17.sh
+++ provisioners/scripts/common/install_aws_corretto_java_jdk_17.sh
@@ -18,8 +18,8 @@
 
 # set amazon corretto 17 installation variables. ---------------------------------------------------
 jdk_home="jdk17"
-jdk_build="17.0.9.8.1"
-jdk_sha256="0cf11d8e41d7b28a3dbb95cbdd90c398c310a9ea870e5a06dac65a004612aa62"
+jdk_build="17.0.10.8.1"
+jdk_sha256="43bc6a5c665bee39a49e63d2c9f78b3141eaca57eebd86982c9bcb601d567ae2"
 jdk_folder="amazon-corretto-${jdk_build}-linux-x64"
 jdk_binary="amazon-corretto-${jdk_build}-linux-x64.tar.gz"
 #jdk_binary="amazon-corretto-${jdk_build:0:2}-x64-linux-jdk.tar.gz"
